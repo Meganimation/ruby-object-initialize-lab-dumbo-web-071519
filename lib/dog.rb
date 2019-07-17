c6e3b9ec0750887ca33e926d4adba7462d59091f
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-17 05:41:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -22,12 +22,12 @@ Don’t forget to add 	.	  def initialize(breed)
 
 class Dog 
   def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
     
   end
   
-  def initialize(breed = "Mutt")
-    @breed = breed 
-  end 
+Dog.new
   
   
 end
