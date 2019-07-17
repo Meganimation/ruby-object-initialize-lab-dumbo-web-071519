9773f8815dd4209ca6b8bca137ff3afa9672f187
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-17 05:38:07 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,30 @@
+Don’t forget to add 	.	  def initialize(breed)
+*     @breed = breed
+*   end
+   ————————————————  Optimized.  class Cat 
+*     
+*       attr_accessor :name, :meow
+*     def meow
+*       puts "meow!"
+*     end
+* end
+*     
+*   
+*   
+*   
+*   maru = Cat.new
+*   maru.name = "Maru"
+*   
+*   puts maru.name
+*   puts maru.meow
+
+
+
+class Dog 
+  
+  def initialize(breed = "Mutt")
+    @breed = breed 
+  end 
+  attr_accessor
+  
+end
